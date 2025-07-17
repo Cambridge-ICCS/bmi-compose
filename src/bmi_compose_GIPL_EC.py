f1bc0afb679d6c24dce936d2f76f904a00121dc2
PMRenaud <rugbyrenaud@gmail.com>
2025-07-17 10:09:02 +0000

    working waves compose notebook

--- src/bmi_compose_GIPL_EC.py
+++ src/bmi_compose_GIPL_EC.py
@@ -1,1 +1,33 @@
-test
+import pymt.models
+import numpy as np
+from bmipy import Bmi
+
+
+ec = pymt.models.ECSimpleSnow()
+gipl = pymt.models.GIPL()
+
+def ectest(bmi1 : Bmi, bmi2 : Bmi, interface : list[str]) -> Bmi:
+
+
+    
+    class ComposedBmi(Bmi):
+
+        def setup(self):
+            ec_defaults = bmi1.setup(".")
+            gipl_defaults = bmi2.setup(".")
+
+
+        def initialize(self, config_file):
+            bmi1.initialize(config_file)
+            bmi2.initialize(config_file)
+            return self
+
+        def update(self, specifics : list[str] , units : dict{str}, set_conversions : dict()):
+            # At most one model with have multiple cycles run
+
+            def bmi1_run():
+              # Run bmi1 cycles
+                bmi1.update()
+
+
+        return self

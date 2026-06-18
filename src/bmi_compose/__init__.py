6b5965fd231491655289756718a38872bdeceb9e
Dominic Orchard <dom.orchard@gmail.com>
2026-06-18 09:35:17 -0600

    update init

--- src/bmi_compose/__init__.py
+++ src/bmi_compose/__init__.py
@@ -1,16 +1,15 @@
 """bmi_compose - Compositional combinators for BMI models."""
 
-from .compose import compose, union, intersection, composeConfig, splitConf, CouplingType
+from .compose import compose_sequentially, union, intersection, composeConfig, splitConf
 from .identity import IdentityBmi
 from .pymt_compose import composed_setup
 
 __all__ = [
-    "compose",
+    "compose_sequentially",
     "union",
     "intersection",
     "composeConfig",
     "splitConf",
-    "CouplingType",
     "IdentityBmi",
     "composed_setup",
 ]
